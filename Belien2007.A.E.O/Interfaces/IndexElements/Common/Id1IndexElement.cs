8184cea36273df1ae331d9a3cede3c116b8eadf3
Justin Britt <britt@uwindsor.ca>
2023-06-15 09:32:43 -0400

    Implement IComparable

--- Belien2007.A.E.O/Interfaces/IndexElements/Common/Id1IndexElement.cs
+++ Belien2007.A.E.O/Interfaces/IndexElements/Common/Id1IndexElement.cs
@@ -1,8 +1,10 @@
 ﻿namespace Belien2007.A.E.O.Interfaces.IndexElements.Common
 {
+    using System;
+
     using Hl7.Fhir.Model;
 
-    public interface Id1IndexElement
+    public interface Id1IndexElement : IComparable<Id1IndexElement>
     {
         /// <summary>
         /// Gets the day.
