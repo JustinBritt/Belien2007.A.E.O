a55f0ecd22fb577644417d44f0ad533f8ec7646b
Justin Britt <britt@uwindsor.ca>
2023-06-20 13:14:29 -0400

    Added abstract factory

---
+++ Belien2007.A.E.O/InterfacesAbstractFactories/IComparersAbstractFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Belien2007.A.E.O.InterfacesAbstractFactories
+{
+    using Belien2007.A.E.O.InterfacesFactories.Comparers;
+
+    public interface IComparersAbstractFactory
+    {
+        IOrganizationComparerFactory CreateOrganizationComparerFactory();
+    }
+}
