dbc5727e0fd93db3551819ae03ed5cf7ba4c3bbe
Justin Britt <britt@uwindsor.ca>
2023-06-20 17:06:43 -0400

    Added visitor

---
+++ Belien2007.A.E.O/InterfacesVisitors/Contexts/ISurgeonNumberTimeBlocksVisitor.cs
@@ -0,0 +1,19 @@
+﻿namespace Belien2007.A.E.O.InterfacesVisitors.Contexts
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using Belien2007.A.E.O.Interfaces.IndexElements.Common;
+    using Belien2007.A.E.O.Interfaces.ParameterElements.Common.SurgeonNumberTimeBlocks;
+
+    public interface ISurgeonNumberTimeBlocksVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+        where TKey : Organization
+        where TValue : INullableValue<int>
+    {
+        RedBlackTree<IsIndexElement, IrParameterElement> RedBlackTree { get; }
+    }
+}
