583403fec57aec3bb7e6ae9e25200f86fe795d48
Justin Britt <britt@uwindsor.ca>
2023-06-20 16:00:03 -0400

    Added visitor

---
+++ Belien2007.A.E.O/InterfacesVisitors/Contexts/IActivePeriodsVisitor.cs
@@ -0,0 +1,19 @@
+﻿namespace Belien2007.A.E.O.InterfacesVisitors.Contexts
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using Belien2007.A.E.O.Interfaces.IndexElements.Common;
+    using Belien2007.A.E.O.Interfaces.ParameterElements.Common.ActivePeriods;
+
+    public interface IActivePeriodsVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+        where TKey : FhirDateTime
+        where TValue : INullableValue<bool>
+    {
+        RedBlackTree<IiIndexElement, IAParameterElement> RedBlackTree { get; }
+    }
+}
---
+++ Belien2007.A.E.O/Visitors/Contexts/ActivePeriodsVisitor.cs
@@ -0,0 +1,55 @@
+﻿namespace Belien2007.A.E.O.Visitors.Contexts
+{
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using Belien2007.A.E.O.Interfaces.IndexElements.Common;
+    using Belien2007.A.E.O.Interfaces.Indices.Common;
+    using Belien2007.A.E.O.Interfaces.ParameterElements.Common.ActivePeriods;
+    using Belien2007.A.E.O.InterfacesFactories.ParameterElements.Common.ActivePeriods;
+    using Belien2007.A.E.O.InterfacesVisitors.Contexts;
+
+    internal sealed class ActivePeriodsVisitor<TKey, TValue> : IActivePeriodsVisitor<TKey, TValue>
+        where TKey : FhirDateTime
+        where TValue : INullableValue<bool>
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ActivePeriodsVisitor(
+            IAParameterElementFactory AParameterElementFactory,
+            Ii i)
+        {
+            this.AParameterElementFactory = AParameterElementFactory;
+
+            this.i = i;
+
+            this.RedBlackTree = new RedBlackTree<IiIndexElement, IAParameterElement>();
+        }
+
+        private IAParameterElementFactory AParameterElementFactory { get; }
+
+        private Ii i { get; }
+
+        public bool HasCompleted => false;
+
+        public RedBlackTree<IiIndexElement, IAParameterElement> RedBlackTree { get; }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            IiIndexElement iIndexElement = this.i.GetElementAt(
+                obj.Key);
+
+            this.RedBlackTree.Add(
+                iIndexElement,
+                this.AParameterElementFactory.Create(
+                    iIndexElement,
+                    obj.Value));
+        }
+    }
+}
