81fe00a0410a53e8d763cefb28ef921481f95d00
Justin Britt <britt@uwindsor.ca>
2023-06-20 12:47:53 -0400

    Added class

---
+++ Belien2007.A.E.O/Classes/Comparers/OrganizationComparer.cs
@@ -0,0 +1,28 @@
+﻿namespace Belien2007.A.E.O.Classes.Comparers
+{
+    using System;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using Belien2007.A.E.O.Interfaces.Comparers;
+
+    internal sealed class OrganizationComparer : IOrganizationComparer
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public OrganizationComparer()
+        {
+        }
+
+        public int Compare(
+            Organization x,
+            Organization y)
+        {
+            return String.CompareOrdinal(
+                x.Id,
+                y.Id);
+        }
+    }
+}
