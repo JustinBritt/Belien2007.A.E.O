94a523623e665c950b035eb42508cd541c0a3c5d
Justin Britt <britt@uwindsor.ca>
2023-06-20 16:22:56 -0400

    Added visitor

---
+++ Belien2007.A.E.O/InterfacesVisitors/Contexts/IDayBedCapacitiesVisitor.cs
@@ -0,0 +1,19 @@
+﻿namespace Belien2007.A.E.O.InterfacesVisitors.Contexts
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using Belien2007.A.E.O.Interfaces.IndexElements.Common;
+    using Belien2007.A.E.O.Interfaces.ParameterElements.Common.DayBedCapacities;
+
+    public interface IDayBedCapacitiesVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+        where TKey : FhirDateTime
+        where TValue : INullableValue<int>
+    {
+        RedBlackTree<IiIndexElement, IcParameterElement> RedBlackTree { get; }
+    }
+}
---
+++ Belien2007.A.E.O/Visitors/Contexts/DayBedCapacitiesVisitor.cs
@@ -0,0 +1,53 @@
+﻿namespace Belien2007.A.E.O.Visitors.Contexts
+{
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using Belien2007.A.E.O.Interfaces.IndexElements.Common;
+    using Belien2007.A.E.O.Interfaces.Indices.Common;
+    using Belien2007.A.E.O.Interfaces.ParameterElements.Common.DayBedCapacities;
+    using Belien2007.A.E.O.InterfacesFactories.ParameterElements.Common.DayBedCapacities;
+    using Belien2007.A.E.O.InterfacesVisitors.Contexts;
+
+    internal sealed class DayBedCapacitiesVisitor<TKey, TValue> : IDayBedCapacitiesVisitor<TKey, TValue>
+        where TKey : FhirDateTime
+        where TValue : INullableValue<int>
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public DayBedCapacitiesVisitor(
+            IcParameterElementFactory cParameterElementFactory,
+            Ii i)
+        {
+            this.cParameterElementFactory = cParameterElementFactory;
+
+            this.i = i;
+        }
+
+        private IcParameterElementFactory cParameterElementFactory { get; }
+
+        private Ii i { get; }
+
+        public bool HasCompleted => false;
+
+        public RedBlackTree<IiIndexElement, IcParameterElement> RedBlackTree { get; }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            IiIndexElement iIndexElement = this.i.GetElementAt(
+                obj.Key);
+
+            this.RedBlackTree.Add(
+                iIndexElement,
+                this.cParameterElementFactory.Create(
+                    iIndexElement,
+                    obj.Value));
+        }
+    }
+}
