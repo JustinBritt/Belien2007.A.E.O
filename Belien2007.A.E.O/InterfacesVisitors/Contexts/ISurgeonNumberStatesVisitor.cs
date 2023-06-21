4ce73b9dc0376ac3425920241e9a3db6c71605da
Justin Britt <britt@uwindsor.ca>
2023-06-21 09:10:29 -0400

    Added visitor

---
+++ Belien2007.A.E.O/InterfacesVisitors/Contexts/ISurgeonNumberStatesVisitor.cs
@@ -0,0 +1,19 @@
+﻿namespace Belien2007.A.E.O.InterfacesVisitors.Contexts
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using Belien2007.A.E.O.Interfaces.IndexElements.Common;
+    using Belien2007.A.E.O.Interfaces.ParameterElements.Stochastic.SurgeonNumberStates;
+
+    public interface ISurgeonNumberStatesVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+        where TKey : Organization
+        where TValue : INullableValue<int>
+    {
+        RedBlackTree<IsIndexElement, IqParameterElement> RedBlackTree { get; }
+    }
+}
---
+++ Belien2007.A.E.O/Visitors/Contexts/SurgeonNumberStatesVisitor.cs
@@ -0,0 +1,55 @@
+﻿namespace Belien2007.A.E.O.Visitors.Contexts
+{
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using Belien2007.A.E.O.Interfaces.IndexElements.Common;
+    using Belien2007.A.E.O.Interfaces.Indices.Common;
+    using Belien2007.A.E.O.Interfaces.ParameterElements.Stochastic.SurgeonNumberStates;
+    using Belien2007.A.E.O.InterfacesFactories.ParameterElements.Stochastic.SurgeonNumberStates;
+    using Belien2007.A.E.O.InterfacesVisitors.Contexts;
+
+    internal sealed class SurgeonNumberStatesVisitor<TKey, TValue> : ISurgeonNumberStatesVisitor<TKey, TValue>
+        where TKey : Organization
+        where TValue : INullableValue<int>
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public SurgeonNumberStatesVisitor(
+            IqParameterElementFactory qParameterElementFactory,
+            Is s)
+        {
+            this.qParameterElementFactory = qParameterElementFactory;
+
+            this.s = s;
+
+            this.RedBlackTree = new RedBlackTree<IsIndexElement, IqParameterElement>();
+        }
+
+        private IqParameterElementFactory qParameterElementFactory { get; }
+
+        private Is s { get; }
+
+        public bool HasCompleted => false;
+
+        public RedBlackTree<IsIndexElement, IqParameterElement> RedBlackTree { get; }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            IsIndexElement sIndexElement = this.s.GetElementAt(
+                obj.Key);
+
+            this.RedBlackTree.Add(
+                sIndexElement,
+                this.qParameterElementFactory.Create(
+                    sIndexElement,
+                    obj.Value));
+        }
+    }
+}
